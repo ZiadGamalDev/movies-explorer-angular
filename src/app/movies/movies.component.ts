0d1e53a8af8e887e75cb982ac2f4635627696b4f
3b3zeem <ahmedzezo2247@gmail.com>
2025-02-09 22:31:40 +0200

    add production_companie & homepage in the interface to show in movieDetails - Edit Movies page Design - make the navbar responsive

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -3,10 +3,12 @@ import { RequestsService } from '../core/service/requests.service';
 import { Movie } from '../core/interface/Movie';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { TruncatePipe } from '../core/pipe/truncate.pipe';
+import { WishlistService } from '../core/service/wishlist.service';
 
 @Component({
   selector: 'app-movies',
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, TruncatePipe],
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.css'],
 })
@@ -14,8 +16,12 @@ export class MoviesComponent implements OnInit {
   movies: Movie[] = [];
   currentPage: number = 1;
   totalPages: number = 1;
+  isInWishlist: boolean = false;
 
-  constructor(private requests: RequestsService) {}
+  constructor(
+    private requests: RequestsService,
+    private _wishlistService: WishlistService
+  ) {}
 
   ngOnInit() {
     this.fetchMovies();
@@ -26,6 +32,18 @@ export class MoviesComponent implements OnInit {
       this.movies = res.results;
       this.totalPages = res.total_pages;
     });
+  }
+
+  toggleWishlist(movie: Movie): void {
+    if (this._wishlistService.isInWishlist(movie.id)) {
+      this._wishlistService.removeFromWishlist(movie.id);
+    } else {
+      this._wishlistService.addToWishlist(movie);
+    }
+  }
+
+  getHeartIconClass(movie: Movie): string {
+    return `fa-heart favorite-icon me-5 ${this._wishlistService.isInWishlist(movie.id) ? 'fa-solid' : 'fa-regular'}`;
   }
 
   goToPage(page: number) {
@@ -40,15 +58,15 @@ export class MoviesComponent implements OnInit {
     let totalToShow = 5;
     let start = Math.max(1, this.currentPage - Math.floor(totalToShow / 2));
     let end = Math.min(this.totalPages, start + totalToShow - 1);
-  
+
     if (end - start < totalToShow - 1) {
       start = Math.max(1, end - totalToShow + 1);
     }
-  
+
     for (let i = start; i <= end; i++) {
       pages.push(i);
     }
-  
+
     return pages;
   }
 
