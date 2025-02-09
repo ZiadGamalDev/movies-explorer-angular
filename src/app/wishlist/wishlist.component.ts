570bce927f2b42bc3ba1ba3beb385b7af5199907
sondosomarr <sondosomarr2020@gmail.com>
2025-02-10 00:11:19 +0200

    add wish-list component

--- src/app/wishlist/wishlist.component.ts
+++ src/app/wishlist/wishlist.component.ts
@@ -1,20 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { WishlistService } from '../core/service/wishlist.service';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Movie } from '../core/interface/Movie';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { TruncatePipe } from "../core/pipe/truncate.pipe";
+import { DatePipe, SlicePipe } from '@angular/common';
+
+
 @Component({
   selector: 'app-wishlist',
-  imports: [],
+  imports: [TruncatePipe, DatePipe,RouterLink,SlicePipe],
   templateUrl: './wishlist.component.html',
   styleUrl: './wishlist.component.css',
 })
 export class WishlistComponent implements OnInit {
   wishlist$: Observable<Movie[]>;
+  
+  movie!: Movie;
   movies: Movie[] = [];
+  isInWishlist: boolean = false;
   constructor(
     private _wishlistService: WishlistService,
     private _router: Router
+    
   ) {
     this._wishlistService.getWishlist().subscribe((movies) => {
       this.movies = movies;
@@ -32,5 +40,14 @@ export class WishlistComponent implements OnInit {
 
   navigateToHome(): void {
     this._router.navigate(['/']);
+  }
+ removeFromWatchList(movieId:number): void {
+    this._wishlistService.removeFromWishlist(movieId);
+    
+  }
+
+  get heartIconClass(): string {
+      return `fa-heart favorite-icon me-5  fa-solid` ;
+    ;
   }
 }
