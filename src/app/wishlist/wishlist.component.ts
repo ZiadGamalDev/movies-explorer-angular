6bbd482098ab16e27fefb14a579b9eab55cddd9a
3b3zeem <ahmedzezo2247@gmail.com>
2025-02-10 11:58:18 +0200

    add rating and date pipes to all website

--- src/app/wishlist/wishlist.component.ts
+++ src/app/wishlist/wishlist.component.ts
@@ -3,27 +3,26 @@ import { WishlistService } from '../core/service/wishlist.service';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Movie } from '../core/interface/Movie';
 import { Router, RouterLink } from '@angular/router';
-import { TruncatePipe } from "../core/pipe/truncate.pipe";
-import { DatePipe, SlicePipe } from '@angular/common';
-import { RatingStarsPipe } from '../pipe/rating-stars.pipe';
-
+import { TruncatePipe } from '../core/pipe/truncate.pipe';
+import { DatePipe } from '@angular/common';
+import { RatingStarsPipe } from '../core/pipe/rating-stars.pipe';
+import { UpToTopComponent } from "../up-to-top/up-to-top.component";
 
 @Component({
   selector: 'app-wishlist',
-  imports: [ DatePipe,RouterLink,SlicePipe,RatingStarsPipe],
+  imports: [DatePipe, RouterLink, RatingStarsPipe, TruncatePipe, UpToTopComponent],
   templateUrl: './wishlist.component.html',
   styleUrl: './wishlist.component.css',
 })
 export class WishlistComponent implements OnInit {
   wishlist$: Observable<Movie[]>;
-  
+
   movie!: Movie;
   movies: Movie[] = [];
   isInWishlist: boolean = false;
   constructor(
     private _wishlistService: WishlistService,
     private _router: Router
-    
   ) {
     this._wishlistService.getWishlist().subscribe((movies) => {
       this.movies = movies;
@@ -42,13 +41,11 @@ export class WishlistComponent implements OnInit {
   navigateToHome(): void {
     this._router.navigate(['/']);
   }
- removeFromWatchList(movieId:number): void {
+  removeFromWatchList(movieId: number): void {
     this._wishlistService.removeFromWishlist(movieId);
-    
   }
 
   get heartIconClass(): string {
-      return `fa-heart favorite-icon me-5  fa-solid` ;
-    ;
+    return `fa-heart favorite-icon me-5  fa-solid`;
   }
 }
